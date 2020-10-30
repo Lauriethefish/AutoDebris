85a3eb9b10fdc72e4225433ec7102f97aa3ffec7
Lauriethefish <44222319+Lauriethefish@users.noreply.github.com>
2020-10-30 18:06:31 +0000

    Changed to Version 0.1.1

--- buildBMBF.ps1
+++ buildBMBF.ps1
@@ -8,4 +8,4 @@ if (-not ($PSVersionTable.PSEdition -eq "Core")) {
 
 & $buildScript NDK_PROJECT_PATH=$PSScriptRoot APP_BUILD_SCRIPT=$PSScriptRoot/Android.mk NDK_APPLICATION_MK=$PSScriptRoot/Application.mk
 
-Compress-Archive -Path "./libs/arm64-v8a/libauto-debris_0_1_0.so", "./extern/libcodegen_0_3_5.so", "./extern/libbeatsaber-hook_0_8_0.so", "./extern/libbeatsaber-hook_0_8_2.so", "./extern/libquestui.so", "./extern/libcustom-types.so", "./bmbfmod.json" -DestinationPath "./auto-debris_v0.1.0.zip" -Update
+Compress-Archive -Path "./libs/arm64-v8a/libauto-debris_0_1_1.so", "./extern/libcodegen_0_3_5.so", "./extern/libbeatsaber-hook_0_8_0.so", "./extern/libbeatsaber-hook_0_8_2.so", "./extern/libquestui.so", "./extern/libcustom-types.so", "./bmbfmod.json" -DestinationPath "./auto-debris_v0.1.1.zip" -Update

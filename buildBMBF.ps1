42b2623a08294d3a2702f385a20be4633b28d69d
Lauriethefish <44222319+Lauriethefish@users.noreply.github.com>
2020-11-14 20:55:49 +0000

    Removed codegen from mod zip - now a core mod.

--- buildBMBF.ps1
+++ buildBMBF.ps1
@@ -8,4 +8,4 @@ if (-not ($PSVersionTable.PSEdition -eq "Core")) {
 
 & $buildScript NDK_PROJECT_PATH=$PSScriptRoot APP_BUILD_SCRIPT=$PSScriptRoot/Android.mk NDK_APPLICATION_MK=$PSScriptRoot/Application.mk
 
-Compress-Archive -Path "./libs/arm64-v8a/libauto-debris_0_1_2.so", "./extern/libcodegen_0_4_0.so", "./extern/libbeatsaber-hook_0_8_4.so", "./extern/libquestui.so", "./extern/libcustom-types.so", "./bmbfmod.json" -DestinationPath "./auto-debris_v0.1.2.zip" -Update
+Compress-Archive -Path "./libs/arm64-v8a/libauto-debris_0_1_2.so", "./extern/libbeatsaber-hook_0_8_4.so", "./extern/libquestui.so", "./extern/libcustom-types.so", "./bmbfmod.json" -DestinationPath "./auto-debris_v0.1.2.zip" -Update

a28caafbf58af167b2c66e71bca78976837fd64d
Lauriethefish <44222319+Lauriethefish@users.noreply.github.com>
2020-11-14 20:29:18 +0000

    Updated libraries.

--- Android.mk
+++ Android.mk
@@ -27,16 +27,16 @@ include $(PREBUILT_SHARED_LIBRARY)
 
 # Build the beatsaber-hook shared library, SPECIFICALLY VERSIONED!
 include $(CLEAR_VARS)
-LOCAL_MODULE := beatsaber-hook_0_8_2
+LOCAL_MODULE := beatsaber-hook_0_8_4
 LOCAL_EXPORT_C_INCLUDES := extern/beatsaber-hook
-LOCAL_SRC_FILES := extern/libbeatsaber-hook_0_8_2.so
+LOCAL_SRC_FILES := extern/libbeatsaber-hook_0_8_4.so
 LOCAL_CPP_FEATURES += exceptions
 include $(PREBUILT_SHARED_LIBRARY)
 # Creating prebuilt for dependency: codegen - version: 0.3.4
 include $(CLEAR_VARS)
-LOCAL_MODULE := codegen_0_3_5
+LOCAL_MODULE := codegen_0_4_0
 LOCAL_EXPORT_C_INCLUDES := extern/codegen
-LOCAL_SRC_FILES := extern/libcodegen_0_3_5.so
+LOCAL_SRC_FILES := extern/libcodegen_0_4_0.so
 include $(PREBUILT_SHARED_LIBRARY)
 # Creating prebuilt for dependency: questui - version: 0.2.2
 include $(CLEAR_VARS)
@@ -57,8 +57,8 @@ LOCAL_SRC_FILES += $(call rwildcard,src/,*.cpp)
 LOCAL_SRC_FILES += $(call rwildcard,extern/beatsaber-hook/src/inline-hook,*.cpp)
 LOCAL_SRC_FILES += $(call rwildcard,extern/beatsaber-hook/src/inline-hook,*.c)
 LOCAL_SHARED_LIBRARIES += modloader
-LOCAL_SHARED_LIBRARIES += beatsaber-hook_0_8_2
-LOCAL_SHARED_LIBRARIES += codegen_0_3_5
+LOCAL_SHARED_LIBRARIES += beatsaber-hook_0_8_4
+LOCAL_SHARED_LIBRARIES += codegen_0_4_0
 LOCAL_SHARED_LIBRARIES += questui
 LOCAL_SHARED_LIBRARIES += custom-types
 LOCAL_LDLIBS += -llog
